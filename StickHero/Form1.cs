20207b32b6aa321c43b93ab9a48c63edd5b39c91
lef <you@example.com>
2023-01-24 23:24:09 +0200

    Нарисовал человечка

---
+++ StickHero/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StickHero
+{
+    public partial class Form1 : Form
+    {
+        StickHeroGame stickHeroGame = new StickHeroGame();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            stickHeroGame.Draw(e.Graphics);
+            e.Graphics.FillRectangle(Brushes.Black, 150, 400, 50, 160);
+        }
+    }
+}
---
+++ StickHero/StickHeroGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace StickHero
+{
+    class StickHeroGame
+    {
+        private Image heroImage = Properties.Resources.Hero;
+
+        public void Draw(Graphics graphics)
+        {
+            graphics.DrawImage(heroImage, 30, 340);
+        }
+    }
+}
